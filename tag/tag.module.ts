cd20ce0b428fbd326a834e03ecc091da150349ea
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-03-03 21:58:30 +0100

    Update tag.module.ts

--- tag/tag.module.ts
+++ tag/tag.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
-
+import { TagController } from "./tag.controller";
+//depencency injection (splitting the application into modules )
 @Module({
-   
+   controllers: [TagController]
 })
 export class TagModule {}
