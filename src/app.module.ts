f23218c14fb177a6c23877406110ed771b999fbc
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-03-27 14:40:22 +0100

    Connection  to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from '@app/app.service';
-import { TagModule } from 'tag/tag.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      logging: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
