ffd7b248dc533b7d4b4b49b772093f4cdaec7abb
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2024-04-04 14:32:03 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
