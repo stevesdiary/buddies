78847b792f6edcea5864a04a7a8f7d5efdf12185
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-05 18:58:33 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 require ('dotenv').config();
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { sequelize } from './models';  // Import sequelize instance
 
-// import userRoutes from './routes/userRoutes';
+import userRoutes from './routes/userRoutes';
 // import chatRoutes from './routes/chatRoutes';
 // import aiChatRoutes from './routes/aiChatRoutes';
 
@@ -16,6 +16,7 @@ const app: Application = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(userRoutes);
 
 // Test MySQL Connection
 sequelize.authenticate()
@@ -27,8 +28,10 @@ sequelize.sync({ alter: true }).then(() => {
   console.log('Database synchronized');
 });
 
-// Routes
-// app.use('/api/users', userRoutes);
+app.use('/', (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'Service running well.'})
+})
+// app.use('/users', userRoutes);
 // app.use('/api/chats', chatRoutes);
 // app.use('/api/ai', aiChatRoutes);
 
