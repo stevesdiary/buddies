4838a87b05b7f76606eef0270f795dbbb29353d9
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-06 03:55:38 +0100

    Update userRoutes.ts
    Add delete, find one, find many users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 const router = Router();
 import userController from "../controllers/userController";
 
-router.post('/user/create', userController.createUser);
+router.post('/create', userController.createUser);
 
-router.get('/alluser', userController.getAllUsers);
+router.get('/all', userController.getAllUsers);
 
-export default { router };
+router.get('/one/:email', userController.getOneUser);
+
+router.delete("/delete/:email", userController.deleteUser);
+
+export default router;
