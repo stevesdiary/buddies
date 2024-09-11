ca0326da2535ca24b6f8ebd741e5fb2a42626cd7
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-11 14:22:38 +0100

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 const router = Router();
 import userController from "../controllers/userController";
-import { validate } from "../middlewares/validator-middleware";
+import validate from "../middlewares/validator-middleware";
 import  schemas  from "../validators/data.schema";
 
 router.post('/create', validate(schemas.userSchema, 'body'), userController.createUser);
 
 router.get('/all', userController.getAllUsers);
 
-router.get('/one/:user_id', validate(schemas.idSchema, 'body'), userController.getOneUser);
+router.get('/one/:user_id', validate(schemas.idSchema, 'params'), userController.getOneUser);
 
 router.put('/update/:user_id', validate(schemas.idSchema, 'params'), userController.updateUser);
 
