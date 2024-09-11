2d9bbdbf72f3fe88d3c2ef9ebdfa166c1f90c8b7
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-11 14:25:37 +0100

    Change DB logging to false

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,4 +6,5 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  logging: false,
 });
