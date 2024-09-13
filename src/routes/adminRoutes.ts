8f3262f535095c7e6f12ba01046b6d3344597b86
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-13 14:50:10 +0100

    Update adminRoutes.ts

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,15 +2,17 @@ import adminController from '../controllers/adminController';
 import { Router } from 'express';
 import router from './userRoutes';
 const app = Router();
+import validate from '../middlewares/validator-middleware';
+import schemas from "../validators/data.schema"
 
-app.post('/create', adminController.createAdmin);
+app.post('/create', validate(schemas.adminSchema, "body"), adminController.createAdmin);
 
 app.get("/all", adminController.findAllAdmin);
 
-app.get("/one", adminController.findAdmin);
+app.get("/one", validate(schemas.adminIdSchema, "params"), adminController.findAdmin);
 
-app.put('/update/:admin_id', adminController.updateAdmin);
+app.put('/update/:admin_id', validate(schemas.adminIdSchema, "params"), validate(schemas.adminSchema, "body"), adminController.updateAdmin);
 
-app.delete('/delete/admin_id', adminController.deleteAdmin);
+app.delete('/delete/admin_id', validate(schemas.adminIdSchema, "params"), adminController.deleteAdmin);
 
 export default router;
