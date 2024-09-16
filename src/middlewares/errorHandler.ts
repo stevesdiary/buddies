d65aae3ad1276c2851f75bf261eb59a8e3f3b4c7
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-16 22:36:24 +0100

    Create Error handler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+
+interface CustomError extends Error {
+  statusCode?: number;
+  details?: any;
+  errors?: any;
+}
+
+const errorHandler = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(error);
+
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'Something went wrong!';
+  let details = null;
+
+  switch (error.name) {
+    case 'ValidationError':
+      statusCode = 400;
+      details = error.details;
+      break;
+    case 'SequelizeUniqueConstraintError':
+      statusCode = 400;
+      message = 'Unique constraint violation';
+      details = error.errors;
+      break;
+  }
+
+  res.status(statusCode).json({
+    status: 'error',
+    message,
+    details,
+    error: error.name
+  });
+};
+
+export default errorHandler;
