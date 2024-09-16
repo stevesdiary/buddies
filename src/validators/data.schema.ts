a40aba7fab08c741cb04c3ad2f1a9ff3e92d34cf
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-16 01:51:22 +0100

    Add Admin schema to create and verify id

--- src/validators/data.schema.ts
+++ src/validators/data.schema.ts
@@ -2,8 +2,8 @@ import Joi from 'joi';
 const roles = process.env.ROLES?.split('|') || ''
 
 
-const adminSchema = Joi.object ({
-	admin_id: Joi.string().required().uuid({ version: 'uuidv4' }).label('Admin_id'),
+const createAdminSchema = Joi.object ({
+	admin_id: Joi.string().optional().uuid({ version: 'uuidv4' }).label('Admin_id'),
 	first_name: Joi.string().min(3).max(20).required().label('First_name'),
 	last_name: Joi.string().min(3).max(20).required().label('Last_name'),
 	username: Joi.string().alphanum().min(3).max(20).required().label('Username'),
@@ -17,8 +17,8 @@ const adminSchema = Joi.object ({
 	role: Joi.string().required().valid(...roles).label('Role')
 });
 
-const userSchema = Joi.object({
-	user_id: Joi.string().required().uuid({ version: 'uuidv4' }).label('user_id'),
+const createUserSchema = Joi.object({
+	user_id: Joi.string().optional().uuid({ version: 'uuidv4' }).label('user_id'),
 	first_name: Joi.string().min(3).max(20).required().label('First_name'),
 	last_name: Joi.string().min(3).max(20).required().label('Last_name'),
 	username: Joi.string().alphanum().min(3).max(20).required().label('Username'),
@@ -27,17 +27,65 @@ const userSchema = Joi.object({
     .pattern(/^(?:\+?234|0)\d{10}$/)
     .required()
     .label('Phone number'),
-	password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).label('Password'),
+	password: Joi.string()
+    .min(8)
+    .max(35)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])/)
+    .messages({
+      'string.min': 'Password must be at least {#limit} characters long',
+      'string.max': 'Password must not exceed {#limit} characters',
+      'string.pattern.base': 'Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character (@.#$!%*?&^)'
+    })
+    .required(),
 	confirmPassword: Joi.ref('password'),
 	country: Joi.string().required().label('Country'),
 	state_or_province: Joi.string().label('State or Province'),
 	state_of_origin: Joi.string().required().label('state of origin'),
 	gender: Joi.string().required().label('Gender'),
-	date_of_birth: Joi.number().min(2006)
-	
+	date_of_birth: Joi.date().iso().required().label('Date of Birth'),
+	age: Joi.number().optional().label('Age'),
+	education_level: Joi.string().required().label('Education level'),
+	profession: Joi.string().required().label('Profession'),
+	hobbies_and_interests: Joi.array().required().label('Hobbies and interests'),
+	qualities: Joi.array().required().label('Qualities'),
+	subscribed: Joi.boolean().optional()
 });
 
-const idSchema = Joi.object({
-	id: Joi.string().required().uuid({ version: 'uuidv4' }).label('User or Admin id')
+const updateUserSchema = Joi.object({
+	user_id: Joi.string().optional().uuid({ version: 'uuidv4' }).label('user_id'),
+	first_name: Joi.string().min(3).max(20).required().label('First_name'),
+	last_name: Joi.string().min(3).max(20).required().label('Last_name'),
+	username: Joi.string().alphanum().min(3).max(20).required().label('Username'),
+	email: Joi.string().email().required().label('Email'),
+	phone_number: Joi.string()
+    .pattern(/^(?:\+?234|0)\d{10}$/)
+    .required()
+    .label('Phone number'),
+	country: Joi.string().required().label('Country'),
+	state_or_province: Joi.string().label('State or Province'),
+	state_of_origin: Joi.string().required().label('state of origin'),
+	gender: Joi.string().required().label('Gender'),
+	date_of_birth: Joi.date().iso().required().label('Date of Birth'),
+	age: Joi.number().optional().label('Age'),
+	education_level: Joi.string().required().label('Education level'),
+	profession: Joi.string().required().label('Profession'),
+	hobbies_and_interests: Joi.array().required().label('Hobbies and interests'),
+	qualities: Joi.array().required().label('Qualities'),
+	subscribed: Joi.boolean().optional()
+});
+
+const userIdSchema = Joi.object({
+	user_id: Joi.string().uuid().required().messages({
+    'any.required': '"User id" is required',
+    'string.guid': '"User id" must be a valid UUID'
+  }),
 })
-export default { adminSchema, userSchema, idSchema };
+const adminIdSchema = Joi.object({
+	admin_id: Joi.string().uuid().required().messages({
+    'any.required': '"Admin id" is required',
+    'string.guid': '"Admin id" must be a valid UUID'
+  })
+})
+
+export default { createAdminSchema, createUserSchema, userIdSchema, adminIdSchema, updateUserSchema };
+
