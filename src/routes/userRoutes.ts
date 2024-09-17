ef603cd6a36d34cd5d1352dd0a84aa3e7a102829
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-17 15:46:31 +0100

    Reformat the route
    Include the authentications and authorizations of relevant roles to access resources

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,35 @@
 import { Router } from "express";
 const router = Router();
-import authenticate from '../middlewares/authentication';
+import { authenticateUserOrAdmin, authorizeUserOrAdmin } from '../middlewares/auth';
+import{ authenticateUser, authorizeUser, authenticateAdmin, authorizeAdmin } from '../middlewares/authentication';
 import userController from "../controllers/userController";
 import validate from "../middlewares/validator-middleware";
 import  schemas  from "../validators/data.schema";
 
-router.post('/create', validate(schemas.createUserSchema, 'body'), userController.createUser);
+router.post('/create',
+	validate(schemas.createUserSchema, 'body'),
+	userController.createUser);
 
-router.get('/all', userController.getAllUsers);
+router.get('/all', authenticateUserOrAdmin,
+	authorizeUserOrAdmin(['basic', 'premium'], ['master']),
+	userController.getAllUsers);
 
-router.get('/one/:user_id', authenticate, validate(schemas.userIdSchema, 'params'), userController.getOneUser);
+router.get('/one/:user_id', authenticateUserOrAdmin,
+	authorizeUserOrAdmin(['basic', 'premium'], ['master']),
+	validate(schemas.userIdSchema, 'params'),
+	userController.getOneUser);
 
-router.put('/update/:user_id', validate(schemas.userIdSchema, "params"), validate(schemas.updateUserSchema, 'body'), userController.updateUser);
+router.put('/update/:user_id',
+	authenticateAdmin,
+	authorizeAdmin(['master']),
+	validate(schemas.userIdSchema, "params"),
+	validate(schemas.updateUserSchema, 'body'),
+	userController.updateUser);
 
-router.delete('/delete/:user_id', validate(schemas.userIdSchema, 'params'), userController.deleteUser);
+router.delete('/delete/:user_id',
+	authenticateAdmin,
+	authorizeAdmin(['master']),
+	validate(schemas.userIdSchema, 'params'),
+	userController.deleteUser);
 
 export default router;
