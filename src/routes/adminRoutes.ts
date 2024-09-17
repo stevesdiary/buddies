0cd4b159884a7def5d3631d4aff82a8b2ad7e41e
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-17 15:48:24 +0100

    Update adminRoutes.ts
    Add relevant roles to access resources and perform actions

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,17 +1,18 @@
 import adminController from '../controllers/adminController';
 import { Router } from 'express';
 const router = Router();
+import  { authenticateAdmin, authorizeAdmin } from '../middlewares/authentication';
 import validate from '../middlewares/validator-middleware';
 import schemas from "../validators/data.schema"
 
-router.post('/create', adminController.createAdmin);
+router.post('/create', authenticateAdmin, authorizeAdmin(['master']),  adminController.createAdmin);
 
-router.get("/all", adminController.findAllAdmin);
+router.get('/all', authenticateAdmin, authorizeAdmin(['master', 'senior']), adminController.findAllAdmin);
 
-router.get("/one/:admin_id", validate(schemas.adminIdSchema, "params"), adminController.findAdmin);
+router.get('/one/:admin_id', authenticateAdmin, authorizeAdmin(['junior', 'master', 'senior']), validate(schemas.adminIdSchema, "params"), adminController.findAdmin);
 
-router.put('/update/:admin_id', validate(schemas.createAdminSchema, "params"), validate(schemas.createAdminSchema, "body"), adminController.updateAdmin);
+router.put('/update/:admin_id', authenticateAdmin, authorizeAdmin(['master']), validate(schemas.createAdminSchema, "params"), validate(schemas.createAdminSchema, "body"), adminController.updateAdmin);
 
-router.delete('/delete/:admin_id', validate(schemas.adminIdSchema, "params"), adminController.deleteAdmin);
+router.delete('/delete/:admin_id',authenticateAdmin, authorizeAdmin(['master']), validate(schemas.adminIdSchema, "params"), adminController.deleteAdmin);
 
 export default router;
