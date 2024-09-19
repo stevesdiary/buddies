549d29d587d5bb495c7e7d59cdb46f46f3bcbded
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-20 00:56:12 +0100

    Add quality and interest routes

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,8 @@ import { sequelize } from './models';  // Import sequelize instance
 import userRoutes from './routes/userRoutes';
 import adminRoutes from './routes/adminRoutes';
 import loginRoutes from './routes/loginRoutes';
-// import aiChatRoutes from './routes/aiChatRoutes';
+import qualityRoutes from './routes/qualityRoutes';
+import interestRoutes from './routes/interestRoutes';
 
 dotenv.config();
 
@@ -27,15 +28,16 @@ sequelize.authenticate()
   // console.log('Database synchronized');
 // });
 
-// app.use('/', (req: Request, res: Response) => {
-//   return res.status(200).json({ message: 'Service running well.'})
-// })
+
 app.use('/user', userRoutes);
 app.use('/admin/', adminRoutes);
 app.use('/login/', loginRoutes);
-// app.use('/api/ai', aiChatRoutes);
+app.use('/quality/', qualityRoutes);
+app.use('/interest/', interestRoutes);
 
-
+app.use('/', (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'Service running well.'})
+})
 const PORT = process.env.LOCAL_PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
