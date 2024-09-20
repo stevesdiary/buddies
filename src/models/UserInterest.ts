3110511cac1e0383263f499daf22536c3adabbe6
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-20 20:23:59 +0100

    Create UserInterest.ts

---
+++ src/models/UserInterest.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './index';
+import User from './User';
+import Interest from './Interests';
+
+class UserInterest extends Model {}
+
+UserInterest.init({
+  user_id: {
+    type: DataTypes.UUID,
+    references: {
+      model: User,
+      key: 'user_id',
+    },
+  },
+  interest_id: {
+    type: DataTypes.UUID,
+    references: {
+      model: Interest,
+      key: 'interest_id',
+    },
+  },
+}, {
+  sequelize,
+	tableName: 'user_interests',
+  modelName: 'UserInterest',
+  timestamps: false,
+});
+
+export default UserInterest;
