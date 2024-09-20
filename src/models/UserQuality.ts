db761545c8a7c055a984a3832affe26fedbf143f
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-20 20:24:05 +0100

    Create UserQuality.ts

---
+++ src/models/UserQuality.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from './index';
+import User from './User';
+import Quality from './Qualities';
+
+class UserQuality extends Model {}
+
+UserQuality.init({
+  user_id: {
+    type: DataTypes.UUID,
+    references: {
+      model: User,
+      key: 'user_id',
+    },
+  },
+  quality_id: {
+    type: DataTypes.UUID,
+    references: {
+      model: Quality,
+      key: 'quality_id',
+    },
+  },
+}, {
+  sequelize,
+	tableName: 'user_qualities',
+  modelName: 'UserQualities',
+  timestamps: false,
+});
+
+export default UserQuality;
