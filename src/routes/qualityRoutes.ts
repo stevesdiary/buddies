477cfb53dc0b787d01f291eae5e1a029cd238cd9
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-23 22:49:59 +0100

    Update qualityRoutes.ts

--- src/routes/qualityRoutes.ts
+++ src/routes/qualityRoutes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import qualitiesController from "../controllers/qualitiesController";
+import { authenticateAdmin, authorizeAdmin } from "../middlewares/authentication";
+import validate from "../middlewares/validator-middleware";
+import schemas from '../validators/data.schema';
 const router = Router();
 
 
-router.post('/create', qualitiesController.createQuality);
+router.post('/create', validate(schemas.qualitySchema, 'body'), qualitiesController.createQuality);
 
 router.get('/one/:quality_id', qualitiesController.getOneQuality);
 
 router.get('/all', qualitiesController.getQualities);
 
-router.delete('/delete/:quality_id', qualitiesController.deleteQuality);
+router.delete('/delete/:quality_id', authenticateAdmin, authorizeAdmin(['master']), qualitiesController.deleteQuality);
 
 export default router;
