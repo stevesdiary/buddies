1a23824e785b047e9b1f045ed26a95332f10cff0
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-24 09:57:48 +0100

    Add authentication and validation to interest routes

--- src/routes/interestRoutes.ts
+++ src/routes/interestRoutes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import interestController from "../controllers/interestController";
 const router = Router();
+import  validate  from '../middlewares/validator-middleware';
+import schemas from '../validators/data.schema';
+import { authenticateAdmin, authorizeAdmin } from "../middlewares/authentication";
 
+router.post('/create', validate(schemas.interestSchema, 'body'), interestController.createInterest);
 
-router.post('/create', interestController.createInterest);
-
-router.get('/one/:interest_id', interestController.getOneInterest);
+router.get('/one/:interest_id', validate(schemas.interestIdSchema, 'params'), interestController.getOneInterest);
 
 router.get('/all', interestController.getInterests);
 
-router.delete('/delete/:interest_id', interestController.deleteInterest);
+router.delete('/delete/:interest_id', authenticateAdmin, authorizeAdmin(['master']), validate(schemas.interestIdSchema, 'params'), interestController.deleteInterest);
 
 export default router;
