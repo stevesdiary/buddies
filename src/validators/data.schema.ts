396cf002a5b5934bc816674445e78b0623b13adb
stevesdiary <stephenoyeyemi@gmail.com>
2024-09-27 23:28:27 +0100

    Update password pattern to include special characters

--- src/validators/data.schema.ts
+++ src/validators/data.schema.ts
@@ -6,13 +6,13 @@ const createAdminSchema = Joi.object ({
 	admin_id: Joi.string().optional().uuid({ version: 'uuidv4' }).label('Admin_id'),
 	first_name: Joi.string().min(3).max(20).required().label('First_name'),
 	last_name: Joi.string().min(3).max(20).required().label('Last_name'),
-	username: Joi.string().alphanum().min(3).max(20).required().label('Username'),
+	username: Joi.string().alphanum().min(4).max(15).required().label('Username'),
 	phone_number: Joi.string()
     .pattern(/^(?:\+?234|0)\d{10}$/)
     .required()
     .label('Phone number'),
 	email: Joi.string().email().required().label('Email'),
-	password: Joi.string().min(8).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).label('Password'),
+	password: Joi.string().min(8).max(35).pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])/)).label('Password'),
 	confirmPassword: Joi.ref('password'),
 	role: Joi.string().required().valid(...roles).label('Role')
 });
@@ -21,7 +21,7 @@ const createUserSchema = Joi.object({
 	user_id: Joi.string().optional().uuid({ version: 'uuidv4' }).label('user_id'),
 	first_name: Joi.string().min(3).max(20).required().label('First_name'),
 	last_name: Joi.string().min(3).max(20).required().label('Last_name'),
-	username: Joi.string().alphanum().min(3).max(20).required().label('Username'),
+	username: Joi.string().alphanum().min(4).max(15).required().label('Username'),
 	email: Joi.string().email().required().label('Email'),
 	phone_number: Joi.string()
     .pattern(/^(?:\+?234|0)\d{10}$/)
@@ -75,6 +75,29 @@ const updateUserSchema = Joi.object({
 	role: Joi.string().optional().label('Role')
 });
 
+const qualitySchema = Joi.object({
+	quality_id: Joi.string().uuid().optional().label('Quality Id'),
+	quality: Joi.string().required().label('Quality')
+})
+
+const interestSchema = Joi.object({
+	interest_id: Joi.string().uuid().optional().label('Interest Id'),
+	interest: Joi.string().required().label('Interest')
+})
+
+const qualityIdSchema = Joi.object({
+	quality_id: Joi.string().uuid().required().messages({
+		'any.required': '"QualityId" is required',
+		'string.guid': '"QualityId" must be a valid UUID'
+	})
+})
+const interestIdSchema = Joi.object({
+	interest_id: Joi.string().uuid().required().messages({
+		'any.required': '"InterestId" is required',
+		'string.guid': '"InterestId" must be a valid UUID'
+	})
+})
+
 const userIdSchema = Joi.object({
 	user_id: Joi.string().uuid().required().messages({
     'any.required': '"User id" is required',
@@ -88,5 +111,15 @@ const adminIdSchema = Joi.object({
   })
 })
 
-export default { createAdminSchema, createUserSchema, userIdSchema, adminIdSchema, updateUserSchema };
+export default {
+	createAdminSchema,
+	createUserSchema,
+	userIdSchema,
+	adminIdSchema,
+	updateUserSchema,
+	qualityIdSchema,
+	qualitySchema,
+	interestSchema,
+	interestIdSchema
+};
 
