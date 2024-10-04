ab1846fc9326426666ba8be9a2ba52e56051cebf
stevesdiary <stephenoyeyemi@gmail.com>
2024-10-05 00:36:59 +0100

    code cleanup

--- src/routes/uploadRoute.ts
+++ src/routes/uploadRoute.ts
@@ -6,18 +6,21 @@ import { uploadImageToCloudinary } from "../services/mediaUploads";
 import { Readable } from "node:stream";
 
 router.post("/upload", upload.single("image"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
-
   try {
-    const fileBuffer = req.file.buffer;
+		const user_id = req.query.user_id as string;
+    if (!req.file || !user_id) {
+      return res.status(400).json({ message: 'No file or user_id.' });
+    }
+		const fileBuffer = req.file.buffer;
     const stream = Readable.from(fileBuffer);
-    const imageUrl = await uploadImageToCloudinary(stream);
-    res.json({ imageUrl });
+    const imageUrl = await uploadImageToCloudinary(stream, user_id);
+    return res.status(200).json({
+      message: 'Image uploaded successfully!',
+      url: imageUrl,
+    });
   } catch (error) {
-    console.error("Upload error:", error);
-    res.status(500).json({ errorMessage: "Failed to upload image", error });
+		console.log('AN ERROR:', error)
+    return res.status(500).json({ message: 'Error uploading image.', error });
   }
 });
 
