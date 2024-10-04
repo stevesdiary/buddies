92c2a2855ed8b8a8740ec4c344aa11d1bd114ea8
stevesdiary <stephenoyeyemi@gmail.com>
2024-10-05 00:37:30 +0100

    update user dp

--- src/services/mediaUploads.ts
+++ src/services/mediaUploads.ts
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryResponse } from './types';
 import { Readable } from 'stream';
-
+import User from '../models/User';
+import { where } from 'sequelize';
 export async function uploadImageToCloudinary(
-  image: string | Readable
+  image: string | Readable,
+  user_id: string
 ): Promise<string> {
   cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -13,16 +15,17 @@ export async function uploadImageToCloudinary(
 
   try {
     let uploadResult: CloudinaryResponse;
-
+    const publicId = `user_${user_id}_`;
     if (typeof image === 'string') {
-      // If image is a string (file path or URL), upload directly
       uploadResult = await cloudinary.uploader.upload(image, {
         folder: 'uploads',
       }) as CloudinaryResponse;
     } else {
       uploadResult = await new Promise((resolve, reject) => {
         const uploadStream = cloudinary.uploader.upload_stream(
-          { folder: 'uploads' },
+          { folder: 'uploads',
+            public_id: publicId
+          },
           (error, result) => {
             if (error) reject(error);
             else resolve(result as CloudinaryResponse);
@@ -31,7 +34,11 @@ export async function uploadImageToCloudinary(
         image.pipe(uploadStream);
       });
     }
-
+    const user = await User.findByPk(user_id);
+    if (uploadResult.secure_url && user) {
+      await user.update({ where: { dp: uploadResult.secure_url } });
+    }
+      
     return uploadResult.secure_url;
   } catch (error) {
     console.error('Error uploading image to Cloudinary:', error);
